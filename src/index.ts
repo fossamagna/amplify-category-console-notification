5e0d902ec8a55c6f836321eb5619e50a18271ca1
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2022-01-12 21:57:41 +0900

    implement transformCategoryStack function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { $TSContext } from 'amplify-cli-core';
+import { $TSContext, IAmplifyResource } from 'amplify-cli-core';
 import * as path from 'path';
+import { ConsoleNotificationInputState } from './provider-utils/awscloudformation/console-notification-inputs-manager/console-notification-input-state';
+import { generateConsoleNotificationStackTemplate } from './provider-utils/awscloudformation/utils/generate-console-notification-stack-template';
 export * from "./amplify-cli-extensibility-helper/types";
 
 export async function executeAmplifyCommand(context: $TSContext) {
@@ -14,4 +16,15 @@ export async function handleAmplifyEvent(context: $TSContext, args: any) {
   const eventHandlerPath = path.join(eventHandlersDirPath, `handle-${args.event}`);
   const eventHandlerModule = require(eventHandlerPath);
   await eventHandlerModule.run(context, args);
+}
+
+export async function transformCategoryStack(context: $TSContext, resource: IAmplifyResource): Promise<void> {
+  if (canResourceBeTransformed(resource.resourceName)) {
+    generateConsoleNotificationStackTemplate(context, resource.resourceName);
+  }
+}
+
+function canResourceBeTransformed(resourceName: string) {
+  const resourceInputState = new ConsoleNotificationInputState(resourceName);
+  return resourceInputState.cliInputFileExists();
 }
